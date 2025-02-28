16bba290e73cd7194ee33db385e477774b5896d0
fofuhua <101991896+fofuhua@users.noreply.github.com>
2025-02-28 22:24:01 +0800

    Initial commit

---
+++ chatbot.py
@@ -0,0 +1,75 @@
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+CallbackContext)
+import configparser
+import logging
+import redis
+from ChatGPT_HKBU import HKBU_ChatGPT
+
+
+
+global redis1
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+                        password=(config['REDIS']['PASSWORD']),
+                        port=(config['REDIS']['REDISPORT']),
+                        decode_responses=(config['REDIS']['DECODE_RESPONSE']),
+                        username=(config['REDIS']['USER_NAME']))
+    
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    #echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    #dispatcher.add_handler(echo_handler)
+
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_command)) 
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+    #start bot
+    updater.start_polling()
+    updater.idle()
+    
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Helping you helping you.')
+
+def add(update: Update, context: CallbackContext) -> None:
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0] # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg + ' for ' +
+                                  redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+
+def hello_command(update: Update, context: CallbackContext) -> None:
+    try:
+        name = context.args[0]  # 获取用户输入的名字
+        update.message.reply_text(f'Good day, {name}!')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /hello <name>')
+        
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+if __name__ == '__main__':
+    main()
